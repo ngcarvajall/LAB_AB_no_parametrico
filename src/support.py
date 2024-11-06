f4e7e11302e680f4bedb829437abdad6d591df9d
ngcarvajall <ngcarvajall@gmail.com>
2024-11-06 14:56:20 +0100

    empezar

---
+++ src/support.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+def exploracion_dataframe(dataframe, columna_control):
+    """
+    Realiza un análisis exploratorio básico de un DataFrame, mostrando información sobre duplicados,
+    valores nulos, tipos de datos, valores únicos para columnas categóricas y estadísticas descriptivas
+    para columnas categóricas y numéricas, agrupadas por la columna de control.
+
+    Params:
+    - dataframe (DataFrame): El DataFrame que se va a explorar.
+    - columna_control (str): El nombre de la columna que se utilizará como control para dividir el DataFrame.
+
+    Returns: 
+    No devuelve nada directamente, pero imprime en la consola la información exploratoria.
+    """
+    print(f"El número de datos es {dataframe.shape[0]} y el de columnas es {dataframe.shape[1]}")
+    print("\n ..................... \n")
+
+    print(f"Los duplicados que tenemos en el conjunto de datos son: {dataframe.duplicated().sum()}")
+    print("\n ..................... \n")
+    
+    
+    # generamos un DataFrame para los valores nulos
+    print("Los nulos que tenemos en el conjunto de datos son:")
+    df_nulos = pd.DataFrame(dataframe.isnull().sum() / dataframe.shape[0] * 100, columns = ["%_nulos"])
+    display(df_nulos[df_nulos["%_nulos"] > 0])
+    
+    print("\n ..................... \n")
+    print(f"Los tipos de las columnas son:")
+    display(pd.DataFrame(dataframe.dtypes, columns = ["tipo_dato"]))
+    
+    
+    print("\n ..................... \n")
+    print("Los valores que tenemos para las columnas categóricas son: ")
+    dataframe_categoricas = dataframe.select_dtypes(include = "O")
+    
+    for col in dataframe_categoricas.columns:
+        print(f"La columna {col} tiene las siguientes valore únicos:")
+        display(pd.DataFrame(dataframe[col].value_counts()).head())    
+    
+    # como estamos en un problema de A/B testing y lo que realmente nos importa es comparar entre el grupo de control y el de test, los principales estadísticos los vamos a sacar de cada una de las categorías
+    
+    # for categoria in dataframe[columna_control].unique():
+    #     dataframe_filtrado = dataframe[dataframe[columna_control] == categoria]
+    
+    #     print("\n ..................... \n")
+    #     print(f"Los principales estadísticos de las columnas categóricas para el {categoria} son: ")
+    #     display(dataframe_filtrado.describe(include = "O").T)
+        
+    #     print("\n ..................... \n")
+    #     print(f"Los principales estadísticos de las columnas numéricas para el {categoria} son: ")
+    #     display(dataframe_filtrado.describe().T)
