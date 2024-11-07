0e57ad7583a505c47168acdc1050f814685d7bd0
ngcarvajall <ngcarvajall@gmail.com>
2024-11-07 01:38:47 +0100

    funciones

--- src/support.py
+++ src/support.py
@@ -53,19 +53,41 @@ def exploracion_dataframe(dataframe, columna_control):
         display(dataframe_filtrado.describe().T)
 
 def crear_df_grupos(dataframe, columna_grupo, columna_metrica):
+    """
+    Crea variables globales para cada grupo de una columna específica y devuelve una lista con los nombres
+    de las variables globales creadas.
+
+    Parámetros:
+    - dataframe (DataFrame): El DataFrame con los datos.
+    - columna_grupo (str): El nombre de la columna que define los grupos.
+    - columna_metrica (str): El nombre de la columna que contiene la métrica o valores que se quieren analizar.
+
+    Retorna:
+    - lista_nombre_variables (list): Lista con los nombres de las variables globales creadas, una por cada grupo.
+    """
     lista_nombre_variables = []
     for valor in dataframe[columna_grupo].unique():
-        globals()[valor.lower()] = dataframe[dataframe[columna_grupo] == valor] [columna_metrica]
+        globals()[valor.lower()] = dataframe[dataframe[columna_grupo] == valor][columna_metrica]
         lista_nombre_variables.append(valor.lower())
     return lista_nombre_variables
 
 def elegir_test(num_grupos, dependencia, lista_nombres):
+    """
+    Selecciona y ejecuta el test estadístico adecuado basado en el número de grupos y la dependencia entre ellos.
 
-    if num_grupos > 2 and dependencia ==0:
+    Parámetros:
+    - num_grupos (int): Número de grupos a comparar.
+    - dependencia (int): Define si los grupos son dependientes (1) o independientes (0).
+    - lista_nombres (list): Lista con los nombres de las variables globales que contienen los datos de cada grupo.
+
+    Retorna:
+    - None: No devuelve nada directamente, pero imprime el resultado del test estadístico correspondiente.
+    """
+    if num_grupos > 2 and dependencia == 0:
         print(stats.kruskal(*[globals()[variable] for variable in lista_nombres]))
     elif num_grupos == 2 and dependencia == 0:
         print(stats.mannwhitneyu(*[globals()[variable] for variable in lista_nombres]))
-    elif num_grupos == 2 and dependencia == 1: 
+    elif num_grupos == 2 and dependencia == 1:
         print(stats.wilcoxon(*[globals()[variable] for variable in lista_nombres]))
     else:
         print('No es posible identificar un método')
